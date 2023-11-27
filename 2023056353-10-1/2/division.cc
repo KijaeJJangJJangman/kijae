089e6e80b48cad3d2ea393ca903e767b566ad13d
2023056353 <kimkijea7619@hanyang.ac.kr>
2023-11-27 17:43:01 +0900

    csp_assignment

--- 2023056353-10-1/2/division.cc
+++ 2023056353-10-1/2/division.cc
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a = 10;
     int b = 4;
-    double c = static_cast<double>(a/b);
+    double c = static_cast<double>(a)/b;
     cout << c << endl;
     return 0;
 }
