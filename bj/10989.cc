a496b85926b0bdb89e4357b479d9af081fdb8091
2023056353 <kimkijea7619@hanyang.ac.kr>
2024-01-09 21:49:07 +0900

    winter baekjoon

---
+++ bj/10989.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n;
+    cin >> n;
+    int arr[10001] = {0};
+    for(int i = 0 ; i < n; i++){
+        int a;
+        cin >> a;
+        arr[a]++;
+    }
+    for(int i = 1 ; i < 10001 ; i++){
+        for(int j = 0 ; j < arr[i] ; j++){
+            cout << i << "\n";
+        }
+    }
+    return 0;
+}
